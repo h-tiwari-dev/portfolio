858211282c45b894823dbfdd01f5fbd94a0fb43f
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2024-06-01 18:57:34 +0530

    correct all changes

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
                 source: '/',
                 destination: '/home',
                 permanent: true,
-                basePath: true,
+                basePath: false,
             },
         ]);
     }
