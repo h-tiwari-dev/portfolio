d42d96d9826426036e7aaee3552ef05e5cd9f885
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2024-06-01 18:50:26 +0530

    few more changes

--- next.config.js
+++ next.config.js
@@ -2,17 +2,8 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    basePath: "/portfolio",
     images: { unoptimized: true },
-    async redirects() {
-        return ([
-            {
-                source: '/',
-                destination: '/home',
-                permanent: true,
-                basePath: false,
-            },
-        ]);
-    }
 }
 
 module.exports = nextConfig
