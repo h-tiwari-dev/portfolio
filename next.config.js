1355a786328e676cf1709e420f48a19cda7d49a4
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2024-06-01 18:47:03 +0530

    next js changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    images: { unoptimized: true },
     async redirects() {
         return ([
             {
