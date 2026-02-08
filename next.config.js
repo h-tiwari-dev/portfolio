aece218b04ea2fe29ded3b25a1f33a858bb59862
Harsh Tiwari <harshtiwari@Harshs-MacBook-Pro.local>
2026-02-08 11:42:14 +0530

    deploy changes

--- next.config.js
+++ next.config.js
@@ -1,21 +1,27 @@
+const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: { unoptimized: true },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = {
-                ...config.resolve.fallback,
-                fs: false,
-                path: false,
-                os: false,
-                crypto: false,
-                stream: false,
-                buffer: false,
-            };
-        }
-        return config;
-    },
-}
+  reactStrictMode: true,
+  images: { unoptimized: true },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+        crypto: false,
+        stream: false,
+        buffer: false,
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
